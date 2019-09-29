d1a89a32b457ec01e0d72419b3f57923f24268b0
shubhisinghal17 <55138255+shubhisinghal17@users.noreply.github.com>
2019-09-29 16:16:59 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,29 +4,46 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y){
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function() x
-  setInverse <- function(solveMatrix) inv <<- solveMatrix
-  getInverse <- function() inv
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  ## first we set our variable to NULL
+    inv <- NULL
+    ## the set function substitutes the matrix X with the matrix Y, and sets the value
+    ## of inv to null
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+ }
+  ## the function get retrieves the value of X
+    get <- function() x
+    
+    ## setinv sets the variable inv to the value of inverse
+    setinv <- function(inverse) inv <<- inverse
+    
+    ## getinv gets the value of inv
+    getinv <- function() inv
+    ## this las line creates a list of the above functions
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+    
 }
 
 
-## Write a short comment describing this function
+## the cacheSolve function returns the inverse of a matrix, 
+## it first checks to see if there is a cached version, and if not
+##calculates de result and returns it.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  inv <- x$getInverse()
-  if(!is.null(inv)){
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data)
-  x$setInverse(inv)
-  inv      
+  ## it returns a value
+    inv <- x$getinv()
+    if(!is.null(inv)){
+        message("Retrieving cached data")
+        return(inv)
+    }
+    ## if inv is null, it calculates the inverse of the matrix.
+    data <- x$get()
+    inv <- solve(data)  
+    x$setinv(inv)
+    inv
+    ## this returns a matrix that is the inverse of 'x'
+   
 }
